f7f3e38ec64a9292efa7d26a5fb699ede5bd9791
nauvalassidq <nauvalsidiq0427@gmail.com>
2025-11-06 11:21:51 +0700

    push pertama untuk project ini

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head lang="id">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('Login Page', 'Laravel') }}</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    </head>
+
+    <body>
+
+    </body>
+</html>
